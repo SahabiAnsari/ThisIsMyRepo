7eb1b790a9c1e06b3d8faf432c812e3d63721e1d
Sahabi Ansari <thesahabiansari@gmail.com>
2024-11-06 20:10:12 +0530

    Initial Commit

---
+++ PythonCodeForTheDay.py
@@ -0,0 +1,5 @@
+#Wussah this the python code of the day!
+
+print("""Today I learned, how to push my code to 
+      github making a github repo, installing git,
+     introductory git commands, and more!""")
