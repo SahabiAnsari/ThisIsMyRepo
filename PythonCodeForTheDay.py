7996c9013258d3c5bfa1d3eaa31bf5f8895bf34b
Sahabi Ansari <thesahabiansari@gmail.com>
2024-11-06 20:15:53 +0530

    This is the second Commit? Perhaps?

--- PythonCodeForTheDay.py
+++ PythonCodeForTheDay.py
@@ -1,5 +1,8 @@
-#Wussah this the python code of the day!
+#This is my python code of the day!
 
 print("""Today I learned, how to push my code to 
       github making a github repo, installing git,
      introductory git commands, and more!""")
+print("""Better believe I will be learning some 
+      more git tomorrow, too!""")
+
